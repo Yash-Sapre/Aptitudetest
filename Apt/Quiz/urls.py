09c480d76f555c7c110dde6e2fb2f48e45a01cc5
unknown <yss052001@gmail.com>
2021-07-08 00:03:05 +0530

    'meetresult'

--- Apt/Quiz/urls.py
+++ Apt/Quiz/urls.py
@@ -14,4 +14,5 @@ urlpatterns = [
     path('add_parameters',views.add_parameters.as_view(),name='add_parameters'),
     path('add_exam',views.add_exam.as_view(),name='add_exam'),
     path('give_test/<int:pk>',views.give_test.as_view(),name='give_test'),
+    path('view_result/<int:pk>',views.view_result.as_view(),name='view_result'),
 ]
--- Apt/Quiz/views.py
+++ Apt/Quiz/views.py
@@ -4,7 +4,7 @@ from .forms import register_user_form,add_questions_form,add_parameters_form,add
 from django.contrib import messages
 from django.contrib.auth.views import LoginView,LogoutView
 from django.urls import reverse,reverse_lazy
-from .models import exam
+from .models import exam,answers, questions
 
 #Written by Yash
 class dashboard(View):
@@ -29,12 +29,10 @@ class register(View):
             return redirect('Quiz:register')
 
 class login(LoginView):
-
     template_name = 'Quiz/login.html'
-    #success_url = 'Quiz/dashboard'
+
 
 class logout(LogoutView):
-    # success_url = reverse('Quiz/login')
     template_name = 'Quiz/logout.html'
 
 class add_questions(View):
@@ -86,25 +84,57 @@ class add_exam(View):
 class give_test(View):
     def get(self,request,pk):
         exam_selected = exam.objects.get(id = pk)
+        exam_answers = answers.objects.filter(exam__id = pk,user__id = request.user.id)
+        if(len(exam_answers) == 0):
+            exam_questions = exam_selected.exam_questions.all()
+            return render(request,template_name='Quiz/give_test.html',context={'exam_questions':exam_questions,'exam_selected':exam_selected})    
+        else:   
+            return redirect('Quiz:dashboard')
         
-        exam_questions = exam_selected.exam_questions.all()
-        return render(request,template_name='Quiz/give_test.html',context={'exam_questions':exam_questions,'exam_selected':exam_selected})
-
     def post(self,request,pk):
-        
         exam_selected = exam.objects.get(id=pk)
         req_dict = request.POST.dict()
         req_dict.pop('csrfmiddlewaretoken')
-        
         for ques_id,ans in req_dict.items() :
-            #temp = models.Student_Answers.objects.create(user = request.user,exam_link=exam_instance,
-            #question_link = models.Questions.objects.get(id =ques_id),student_answer = ans)
-            print(ques_id)
-            print(ans)
+            temp = answers.objects.create(user = request.user,exam=exam_selected,question = questions.objects.get(id =ques_id),student_answer = ans)
+        return redirect('Quiz:dashboard')
 
-            
-        # return redirect('Quiz:dashboard')
 
+class view_result(View):
+    def get(self,request,pk):
+        exam_selected = exam.objects.get(id=pk)
+        submitted_answers = answers.objects.filter(exam__id = pk,user__id = request.user.id)
+        personality_dict = {'Extraversion':'E','Introversion':'I','Sensing':'S','Intuition':'N','Thinking':'T','Feeling':'F','Judgement':'J','Perception':'P'}
+        personality_dict1 = {'Extraversion':0,'Sensing':1,'Thinking':2,'Judgement':3}
+        personality_dict1_lst = ['Extraversion','Sensing','Thinking','Judgement']
+        personality_dict2 = {'Introversion':0,'Intuition':1,'Feeling':2,'Perception':3}
+        personality_dict2_lst = ['Introversion','Intuition','Feeling','Perception']
+
+        parameters_list=[]
+        
+        para_num_list=[0,0,0,0]
+        final_parameters=['Q','Q','Q','Q']
+        for ans in submitted_answers:
+            if ans.student_answer == ans.question.answer1:
+                parameters_list.append(personality_dict[ans.question.parameter.parameter1])
+                # print(personality_dict1[ans.question.parameter.parameter1])
+                para_num_list[personality_dict1[ans.question.parameter.parameter1]] += 1
+
+            else:
+                parameters_list.append(personality_dict[ans.question.parameter.parameter2])
+                para_num_list[personality_dict2[ans.question.parameter.parameter2]] -= 1
+            parameter_word = ''.join(parameters_list)
+
+        index=0
+        for value in para_num_list:
+
+            if value > 0 :
+                final_parameters[index] = personality_dict1_lst[index]
+            elif value < 0 :
+                final_parameters[index] = personality_dict2_lst[index]
+            index=index+1  
+
+        return render(request,template_name='Quiz/view_result.html',context={'lst':final_parameters})                
     
 
     
