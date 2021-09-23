50b6510362aafb1e8beae4baa3e9987566b14adc
Yash <yss052001@gmail.com>
2021-09-23 20:54:22 +0530

    'BasicReport'

--- Apt/Quiz/views.py
+++ Apt/Quiz/views.py
@@ -1,7 +1,6 @@
+import reportlab.pdfgen.canvas
 from django.shortcuts import render,redirect
 from django.views import View
-from reportlab.platypus.doctemplate import SimpleDocTemplate
-from reportlab.platypus.tables import COLORED_GRID_STYLE
 from .forms import register_user_form,add_questions_form,add_parameters_form,add_exam_form
 from django.contrib import messages
 from django.contrib.auth.views import LoginView,LogoutView
@@ -10,12 +9,12 @@ from .models import exam,answers, questions
 from django.views.generic import DeleteView
 from django.http import FileResponse
 import io
-from reportlab.pdfgen import canvas
-from reportlab.lib.pagesizes import letter
-from reportlab.graphics.shapes import Drawing
-from reportlab.graphics.charts.barcharts import(VerticalBarChart)
-
 import os
+from reportlab.platypus import Paragraph,SimpleDocTemplate
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.lib import colors
+from reportlab.graphics.shapes import Drawing
+from reportlab.graphics.charts.barcharts import VerticalBarChart
 
 
 class dashboard(View):
@@ -141,70 +140,68 @@ class delete_exam(DeleteView):
 
 
 class report(View):
+
     def get(self,request,pk):
+
         exam_selected = exam.objects.get(id=pk)
-        submitted_answers = answers.objects.filter(exam__id = pk,user__id = request.user.id)
+        submitted_answers = answers.objects.filter(exam__id=pk,user__id=request.user.id)
         personality_dict1 = {'Extraversion': 0,'Sensing': 0,'Thinking': 0,'Judgement': 0}
         personality_dict2 = {'Introversion': 0,'Intuition': 0,'Feeling': 0,'Perception': 0}
+
         for ans in submitted_answers:
             if ans.student_answer == ans.question.answer1:
                 personality_dict1[ans.question.parameter.parameter1] += 1
             else:
                 personality_dict2[ans.question.parameter.parameter2] += 1
-        drawing = Drawing(400,200)
-        print(personality_dict1)
-        extraversion_per=personality_dict1['Extraversion']/(personality_dict1['Extraversion']+personality_dict2['Introversion'])*100
-        introversion_per=100-extraversion_per
-        sensing_per=personality_dict1['Sensing']/(personality_dict1['Sensing']+personality_dict2['Intuition'])*100
-        intuition_per=100-sensing_per
-        thinking_per=personality_dict1['Thinking']/(personality_dict1['Thinking']+personality_dict2['Feeling'])*100
-        feeling_per=100-thinking_per
-        judgement_per=personality_dict1['Judgement']/(personality_dict1['Judgement']+personality_dict2['Perception'])*100
-        perception_per=100-judgement_per
-        data = [
-            (extraversion_per,sensing_per,thinking_per,judgement_per),
-            (introversion_per,intuition_per,feeling_per,perception_per)
-        ]
-        bc =VerticalBarChart()
-        bc.x = 100
-        bc.y = 100
-        bc.height = 125
-        bc.width = 300
-        bc.data = data
-        # bc.strokeColor = COLORED_GRID_STYLE.black
+
+        # Calculating percentage for each parameter of first category ESTJ
+        extraversion_percent = personality_dict1['Extraversion']/(personality_dict1['Extraversion']+personality_dict2['Introversion'])*100
+        sensing_percent = personality_dict1['Sensing']/(personality_dict1['Sensing']+personality_dict2['Intuition'])*100
+        thinking_percent = personality_dict1['Thinking']/(personality_dict1['Thinking']+personality_dict2['Feeling'])*100
+        judgement_percent = personality_dict1['Judgement']/(personality_dict1['Judgement']+personality_dict2['Perception'])*100
+
+        # Preparation for pdf and retrieving report text
         buf = io.BytesIO()
         user_id = request.user.id
-        pdf = canvas.Canvas(buf,pagesize='A4')
-        pdf.setFont("Helvetica",14)
-        #The basereport file is fixed and the text files is stored locally
         path = os.getcwd()
         with open(os.path.join(path,'ReportBase.txt'),'r') as file:
             text = file.read()
+        pdf = SimpleDocTemplate(buf)
+        flow = []
+        styles = getSampleStyleSheet()
 
-        # x and y have been set at starting position
-        x = 50
-        y = 770
-        # lines list has been set.
-        lines = ['']
-        words = text.split()
-        line_number = 0
-        # lines list is been create with each line having limit of 78 letters
-        for word in words :
-            if len(lines[line_number] + ' ' + word) < 78 :
-                lines[line_number] = lines[line_number] + ' ' + word
-            else:
-                lines.append('')
-                line_number = line_number + 1
-        # lines are now being printed each line has height of 15 ###Note:y can have 50 lines
-        for line in lines :
-            pdf.drawString(x,y,line)
-            y = y - 15
+        # The code of text part
+        paragraph_text = Paragraph(text,style=styles['Normal'])
+        flow.append(paragraph_text)
 
-        pdf.drawImage(image=drawing,x=400,y=200)
-        pdf.showPage()
-        pdf.save()
-        # buffer has been set at 0th position
+        # The code of vertical bar chart
+        drawing = Drawing(400, 200)
+        data = [
+            (extraversion_percent,sensing_percent,thinking_percent,judgement_percent),
+            (100 - extraversion_percent, 100 - sensing_percent, 100 - thinking_percent, 100 -judgement_percent),
+        ]
+        chart = VerticalBarChart()
+        chart.x = 50
+        chart.y = 50
+        chart.height = 125
+        chart.width = 300
+        chart.data = data
+        chart.strokeColor = colors.black
+        chart.valueAxis.valueMin = 0
+        chart.valueAxis.valueMax = 100
+        chart.valueAxis.valueStep = 10
+        chart.categoryAxis.labels.boxAnchor = 'ne'
+        chart.categoryAxis.labels.dx = 8
+        chart.categoryAxis.labels.dy = -2
+        chart.categoryAxis.labels.angle = 30
+        chart.categoryAxis.categoryNames = ['E/I', 'S/I', 'T/F','J/P']
+        drawing.add(chart)
+        flow.append(drawing)
+
+        # Building the final pdf
+        pdf.build(flow)
         buf.seek(0)
+
         return FileResponse(buf, as_attachment=True,filename=f"Report {user_id}-{pk}.pdf")
     
     
